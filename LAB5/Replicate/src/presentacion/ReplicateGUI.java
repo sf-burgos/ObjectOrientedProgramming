cee0183a90b05d3c8bbb43483c94d7ab3360cad6
DanielAlfonso17 <daniel.alfonso-b@mail.escuelaing.edu.co>
2019-11-04 19:33:26 -0500

    Adelantando lab 5

--- LAB5/Replicate/src/presentacion/ReplicateGUI.java
+++ LAB5/Replicate/src/presentacion/ReplicateGUI.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+import javax.swing.JColorChooser;
 import java.io.File;
 import java.util.*;
 
@@ -18,16 +19,25 @@ public class ReplicateGUI extends JFrame{
 	private JMenuItem salir;
 	/**Paneles */ 
 	private JPanel principal;
+	private JPanel tablero;
 	/**Botones */ 
 	private JButton replicar;
 	private JButton numeroReplicas;
 	private JButton terminarReplica;
 	private JButton cambiarColor;
 	private JButton cambiarDimension;
-	private JButton[][] matrizBotones = new JButton[10][10];
+	private JButton refrescar;
+	private JButton[][] matrizBotones;
 	
+	/**Colores */
+	private Color colorAnteriorM;
+	private Color colorAnteriorR;
 	/**Constructor */ 
-	private ReplicateGUI(){		
+	private ReplicateGUI(){	
+		principal = new JPanel();
+		tablero = new JPanel();
+		colorAnteriorM = Color.white;
+		colorAnteriorR = Color.black;
 		prepareElementos();		
 		prepareAcciones();
 
@@ -44,7 +54,7 @@ public class ReplicateGUI extends JFrame{
 		Dimension dim=super.getToolkit().getScreenSize();
 		this.setMinimumSize(new Dimension(dim.width/2, dim.height/2));
 		prepareElementosMenu();
-		prepareElementosTablero();
+		prepareElementosTablero(10,10);
 	}
 	public void prepareElementosMenu(){
 		menu=new JMenuBar();
@@ -96,17 +106,36 @@ public class ReplicateGUI extends JFrame{
 				}
 			}
 		});
-		for (int i=0;i<10;i++){
-			for(int j=0;j<10;j++){
+		for (int i=0;i<matrizBotones.length;i++){
+			for(int j=0;j<matrizBotones[0].length;j++){
 				matrizBotones[i][j].addActionListener(new ActionListener(){
-					public void actionPerformed(ActionEvent e){
+					public void actionPerformed(ActionEvent e){						
 						JButton evento = (JButton)e.getSource();
 						evento.setBackground(Color.BLACK);
 					}
 				});
 			}
 		}
-
+		refrescar.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				refresh();
+			}
+		});
+		cambiarColor.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				cambiarLosColores();
+			}
+		});
+		cambiarDimension.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				String filas = JOptionPane.showInputDialog(null,"Ingrese el numero de filas:","Cambio de dimension",JOptionPane.QUESTION_MESSAGE);
+				String columnas = JOptionPane.showInputDialog(null,"Ingrese el numero de columnas:","Cambio de dimension",JOptionPane.QUESTION_MESSAGE);
+				int numeroFilas,numeroColumnas;
+				numeroFilas = Integer.parseInt(filas);
+				numeroColumnas = Integer.parseInt(columnas);
+				cambiarDimensionTablero(numeroFilas,numeroColumnas);
+			}
+		});
 	}
 	
 	public void salga(){
@@ -118,15 +147,17 @@ public class ReplicateGUI extends JFrame{
 	
 	/**Iniciar tablero */ 
 	
-	public JPanel prepareElementosTablero(){
-		principal = new JPanel();
+	public JPanel prepareElementosTablero(int filas,int columnas){
+		//principal = new JPanel();
+		matrizBotones = new JButton[filas][columnas];
 		setContentPane(principal);
 		principal.setLayout(new BorderLayout());	
-		JPanel tablero = new JPanel();
-		tablero.setLayout(new GridLayout(10,10));		
-		for(int i=0; i<10;i++){
-			for(int j=0;j<10;j++){
-				matrizBotones[i][j] = new JButton();	
+		//JPanel tablero = new JPanel();
+		tablero.setLayout(new GridLayout(filas,columnas));		
+		for(int i=0; i<matrizBotones.length;i++){
+			for(int j=0;j<matrizBotones[0].length;j++){
+				matrizBotones[i][j] = new JButton();
+				matrizBotones[i][j].setBackground(Color.white);
 				tablero.add(matrizBotones[i][j]);
 			}
 		}
@@ -137,17 +168,60 @@ public class ReplicateGUI extends JFrame{
 		terminarReplica = new JButton("Terminar Replicar");
 		cambiarColor = new JButton("Cambiar Color");
 		cambiarDimension = new JButton("Cambiar Dimension");
+		refrescar = new JButton("Refrescar");
 		botones.setLayout(new GridLayout(2,3));
 		botones.add(replicar);
 		botones.add(numeroReplicas);
 		botones.add(terminarReplica);
 		botones.add(cambiarColor);
 		botones.add(cambiarDimension);
+		botones.add(refrescar);
 		principal.add(botones, BorderLayout.SOUTH);		
 		return principal;
 	}
 	
-
+	public void refresh(){
+		colorAnteriorM = Color.white;
+		colorAnteriorR = Color.black;
+		for(int i=0; i<matrizBotones.length;i++){
+			for(int j=0;j<matrizBotones[0].length;j++){				
+				tablero.remove(matrizBotones[i][j]);
+				matrizBotones[i][j] = new JButton();
+				matrizBotones[i][j].setBackground(colorAnteriorM);
+				tablero.add(matrizBotones[i][j]);
+			}
+		}
+		prepareAcciones();
+	}
+	
+	public void cambiarLosColores(){
+		Color colorNuevoM = JColorChooser.showDialog(null,"Seleccione color para la matriz",colorAnteriorM);
+		Color colorNuevoR = JColorChooser.showDialog(null,"Seleccione color para los cuadros a replicar",colorAnteriorR);
+		for(int i=0;i<matrizBotones.length;i++){
+			for(int j=0; j<matrizBotones[0].length;j++){
+				if(matrizBotones[i][j].getBackground() == colorAnteriorM){
+					matrizBotones[i][j].setBackground(colorNuevoM);
+				}else{
+					matrizBotones[i][j].setBackground(colorNuevoR);
+				}
+			}
+		}
+		colorAnteriorM = colorNuevoM;
+		colorAnteriorR = colorNuevoR;
+		
+	}
+	
+	public void cambiarDimensionTablero(int filas,int columnas){
+		colorAnteriorM = Color.white;
+		colorAnteriorR = Color.black;
+		for(int i=0; i<matrizBotones.length;i++){
+			for(int j=0;j<matrizBotones[0].length;j++){				
+				tablero.remove(matrizBotones[i][j]);		
+			}
+		}
+		prepareElementosTablero(filas,columnas);
+		prepareAcciones();
+	}
 
 
 }
